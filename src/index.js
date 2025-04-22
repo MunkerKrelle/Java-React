0978aa8084346c36815098971729691561615356
Kristian Asbjørn <krasnielsen@gmail.com>
2025-04-22 10:43:45 +0200

    huhio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client";
+import GetName from "./App"; // Import your App component
+
+// Render the App component
+const root = createRoot(document.getElementById("root"));
+root.render(<GetName />);
