598b3fe19cc33755f5eaa6dd2befb97e089860e0
Zanzicki <144442443+Zanzicki@users.noreply.github.com>
2025-04-24 14:32:05 +0100

    Added moar files

---
+++ src/components/topBar/topBar.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react"
+import "./topbar.css"
+
+export default function TopBar(){
+    return(
+        <div className="topBarContainer">
+            <div className="topBarLeft"></div>
+            <div className="topBarCenter"></div>
+            <div className="topBarRight"></div>
+
+
+        </div>
+    )
+}
